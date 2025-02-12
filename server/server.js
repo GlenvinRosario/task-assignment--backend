7dd27e4b96b4fb2db36e7529c682d826cac9bd77
apple <apple@Glenvin-Rosario.local>
2025-02-12 17:28:40 +0530

    enhanced code

--- server/server.js
+++ server/server.js
@@ -16,8 +16,8 @@ const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ MongoDB Connected Successfully"))
-  .catch((err) => console.error("❌ MongoDB Connection Failed", err));
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.error("MongoDB Connection Failed", err));
 
 // app.use(authenticate)
 app.use("/api/auth", authRoutes); 
