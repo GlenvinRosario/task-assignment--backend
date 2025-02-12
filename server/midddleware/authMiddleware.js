a7419183424ba56910d9926cf1e61aa415506be5
apple <apple@Glenvin-Rosario.local>
2025-02-12 17:10:00 +0530

    added middleware

--- server/midddleware/authMiddleware.js
+++ server/midddleware/authMiddleware.js
@@ -1,15 +1,3 @@
-// const jwt = require("jsonwebtoken");
-
-// exports.authenticate = (req, res, next) => {
-//   const token = req.headers.authorization;
-//   if (!token) return res.status(401).json({ error: "Unauthorized" });
-
-//   jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (err, decoded) => {
-//     if (err) return res.status(401).json({ error: "Invalid token" });
-//     req.userId = decoded.userId;
-//     next();
-//   });
-// };
 
 const jwt = require("jsonwebtoken");
 
@@ -19,7 +7,7 @@ exports.authenticate = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
-    req.userId = decoded.userId; // Attach userId to the request
+    req.userId = decoded.userId; 
     next();
   } catch (error) {
     return res.status(401).json({ error: "Invalid token" });
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const authRoutes = require("./routes/authRoutes");
 const listRoutes = require("./routes/listRoutes");
 const searchRoutes = require('./routes/search.js')
+const  authenticate = require('./midddleware/authMiddleware.js')
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -18,6 +19,7 @@ mongoose
   .then(() => console.log("✅ MongoDB Connected Successfully"))
   .catch((err) => console.error("❌ MongoDB Connection Failed", err));
 
+app.use(authenticate)
 app.use("/api/auth", authRoutes); 
 app.use("/", searchRoutes);
 app.use("/api/lists",listRoutes);
